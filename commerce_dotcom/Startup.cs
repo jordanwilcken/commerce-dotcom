65178b381559525f54cb9265f3a9313fc801a8c1
jordanwilcken <jordanwilcken@gmail.com>
2017-06-17 07:05:47 -0600

    commerce.com - now featuring Nancy!

--- commerce_dotcom/Startup.cs
+++ commerce_dotcom/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Nancy.Owin;
 
-namespace commerce_dotcom
+namespace again
 {
     public class Startup
     {
@@ -28,10 +24,7 @@ namespace commerce_dotcom
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseOwin(pipeline => pipeline.UseNancy());
         }
     }
 }
---
+++ commerce_dotcom/nancy-modules/RootModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace commerce_dotcom.nancy_modules
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule()
+        {
+            Get["/"] = _ => Response.AsRedirect("content/index.html");
+        }
+    }
+}
